914837f4ccf2270046727f737d8fa47b6e80c3f4
john ross <johnhoundji@gmail.com>
2025-11-04 21:17:53 +0100

    finalisation du module analyzer

--- src/analyzer.py
+++ src/analyzer.py
@@ -3,7 +3,16 @@ from pprint import pprint
 from textblob_fr import PatternTagger, PatternAnalyzer
 from textblob import Blobber
 
-# Crée un Blobber FR
+
+def polarity(polarity):
+    if polarity>0.1:
+        return "positif"
+    if polarity<-0.1:
+       return"negatif"
+    else:
+        return "neutre"
+
+
 tb = Blobber(pos_tagger=PatternTagger(), analyzer=PatternAnalyzer())
 
 def analyst():
@@ -11,17 +20,24 @@ def analyst():
     ma_liste3 = []
 
     for element in new_list2:
+        
         text = element["reviews_text"]
-        if not text.strip():  # Gestion des avis vides
-            sentiment = {'polarity': 0.0, 'subjectivity': 0.0}
+        if not text.strip():  
+            sentiment = {'polarity': 0.0, 'subjectivity': 0.0,'label':'neutre'}
         else:
             blob = tb(text)
             sentiment = {'polarity': blob.sentiment[0], 'subjectivity': blob.sentiment[1]}
+            sentiment['label'] = polarity(blob.sentiment[0]) 
 
         element['sentiment'] = sentiment
+
         ma_liste3.append(element)
+        
+
 
     return ma_liste3
 
 resultats = analyst()
 pprint(resultats)
+
+
